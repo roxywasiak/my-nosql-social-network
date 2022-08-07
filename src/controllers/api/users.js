89973f9d241b8a7010b6fd4bb728ff5ea339f20c
roxywasiak <ruksclone@hotmail.com>
2022-08-07 19:23:04 +0100

    added getallusers and getuserbyid contollers

--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -3,12 +3,25 @@ const { User, Thoughts } = require("../../models");
 
 const getAllUsers = async (req, res) => {
   try {
-  } catch {}
+    const users = await User.find({});
+
+    return res.json({ data: users });
+  } catch (error) {
+    console.log(`[Error]: Failed to get all users | ${error.message}`);
+  }
 };
 
 const getUserById = async (req, res) => {
   try {
-  } catch {}
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ success: false });
+    }
+    return res.json({ data: user });
+  } catch (error) {
+    console.log(`[Error]: Failed to get user | ${error.message}`);
+  }
 };
 
 const createNewUser = async (req, res) => {
--- src/seeds/index.js
+++ src/seeds/index.js
@@ -8,39 +8,51 @@ const thoughts = require("./thoughts.json");
 //init function
 //connect to db
 const init = async () => {
-    try {
-      const DB_NAME = process.env.DB_NAME;
-      const MONGODB_URI =
-        process.env.MONGODB_URI || `mongodb://localhost:27017/${DB_NAME}`;
-  
-      const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      };
-  
-      await mongoose.connect(MONGODB_URI, options);
-  
-      console.log("[INFO]: Connected to DB successful");
+  try {
+    const DB_NAME = process.env.DB_NAME;
+    const MONGODB_URI =
+      process.env.MONGODB_URI || `mongodb://localhost:27017/${DB_NAME}`;
 
-//delete
-//insert
-//users/ thoughts
-await User.deleteMany({});
-await User.insertMany(users);
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
 
-console.log("[INFO]: Successfully seeded users");
+    await mongoose.connect(MONGODB_URI, options);
 
-await Thought.deleteMany({});
-await Thought.insertMany(thoughts);
+    console.log("[INFO]: Connected to DB successful");
 
-console.log("[INFO]: Successfully seeded thoughts");
+    //delete
+    //insert
+    //users/ thoughts
+    await User.deleteMany({});
+    await User.insertMany(users);
 
+    console.log("[INFO]: Successfully seeded users");
 
-//seed thoughts with the users
+    await Thoughts.deleteMany({});
+    await Thoughts.insertMany(thoughts);
 
-//exit
+    console.log("[INFO]: Successfully seeded thoughts");
 
+    const usersFromDb = await User.find({});
+    const thoughtsFromDb = await Thoughts.find({});
+    //seed thoughts with the users
+    const thoughtPromises = thoughtsFromDb.map(async (thought) => {
+      const username = thought.username;
 
+      const user = usersFromDb.find((user) => user.username === username);
 
+      user.thoughts.push(thought._id.toString());
+      await User.findByIdAndUpdate(user._id, { ...user });
+    });
+    await Promise.all(thoughtPromises);
+    console.log("[INFO]: Successfully seeded thoughts");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to seed DB | ${error.message}`);
+  }
+  //exit
+  process.exit(0);
+};
 //call init
 init();
